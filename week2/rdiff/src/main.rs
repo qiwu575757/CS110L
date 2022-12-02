6bd37341aa61d4fd5e8ec12a54ccfd9d8eb43826
Qi Wu <2193224374@qq.com>
2022-12-02 20:40:07 +0800

    finish week2

--- week2/rdiff/src/main.rs
+++ week2/rdiff/src/main.rs
@@ -3,34 +3,73 @@ use std::env;
 use std::fs::File; // For read_file_lines()
 use std::io::{self, BufRead}; // For read_file_lines()
 use std::process;
+use std::cmp;
 
 pub mod grid;
 
 /// Reads the file at the supplied path, and returns a vector of strings.
-#[allow(unused)] // TODO: delete this line when you implement this function
 fn read_file_lines(filename: &String) -> Result<Vec<String>, io::Error> {
-    unimplemented!();
-    // Be sure to delete the #[allow(unused)] line above
+    let mut result = Vec::new();
+
+    // use ? is better than unwrap()
+    let file = File::open(filename)?;
+    for line in io::BufReader::new(file).lines() {
+        let line_str = line?;
+
+        // do something with line_str
+        result.push(line_str);
+    }
+    Ok(result)
 }
 
-#[allow(unused)] // TODO: delete this line when you implement this function
 fn lcs(seq1: &Vec<String>, seq2: &Vec<String>) -> Grid {
     // Note: Feel free to use unwrap() in this code, as long as you're basically certain it'll
     // never happen. Conceptually, unwrap() is justified here, because there's not really any error
     // condition you're watching out for (i.e. as long as your code is written correctly, nothing
     // external can go wrong that we would want to handle in higher-level functions). The unwrap()
     // calls act like having asserts in C code, i.e. as guards against programming error.
-    unimplemented!();
-    // Be sure to delete the #[allow(unused)] line above
+    
+    // get string size
+    let s1_len = seq1.len();
+    let s2_len = seq2.len();
+    
+    let mut grid = Grid::new(s1_len+1, s2_len+1);
+    
+    // set
+    for i in 0..(s1_len+1) {
+        grid.set(i, 0, 0).unwrap();
+    }
+    for j in 0..(s2_len+1) {
+        grid.set(0, j, 0).unwrap();
+    }
+    for i in 0..s1_len {
+        for j in 0..s2_len {
+            if seq1[i] == seq2[j] {
+                grid.set(i+1, j+1, grid.get(i, j).unwrap() + 1).unwrap();
+            } else {
+                grid.set(i+1, j+1, cmp::max(grid.get(i+1, j).unwrap(), grid.get(i, j+1).unwrap())).unwrap();
+            }
+        }
+    }
+
+    grid
 }
 
-#[allow(unused)] // TODO: delete this line when you implement this function
 fn print_diff(lcs_table: &Grid, lines1: &Vec<String>, lines2: &Vec<String>, i: usize, j: usize) {
-    unimplemented!();
-    // Be sure to delete the #[allow(unused)] line above
+    if i > 0 && j > 0 && lines1[i-1] == lines2[j-1] {
+        print_diff(lcs_table, lines1, lines2, i-1, j-1);
+        println!("  {}", lines1[i-1]);
+    } else if j > 0 && (i == 0 || lcs_table.get(i, j-1).unwrap() >= lcs_table.get(i-1, j).unwrap()) {
+        print_diff(lcs_table, lines1, lines2, i, j-1);
+        println!("> {}", lines2[j-1]);
+    } else if i > 0 && (j == 0 || lcs_table.get(i, j-1).unwrap() < lcs_table.get(i-1, j).unwrap()) {
+        print_diff(lcs_table, lines1, lines2, i-1, j);
+        println!("< {}", lines1[i-1]);
+    } else {
+        println!("");
+    }
 }
 
-#[allow(unused)] // TODO: delete this line when you implement this function
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
@@ -40,8 +79,11 @@ fn main() {
     let filename1 = &args[1];
     let filename2 = &args[2];
 
-    unimplemented!();
-    // Be sure to delete the #[allow(unused)] line above
+    let lines1 = read_file_lines(filename1).expect("Read error at file 1.");
+    let lines2 = read_file_lines(filename2).expect("Read error at file 2.");
+
+    let lcs_table = lcs(&lines1, &lines2);
+    print_diff(&lcs_table, &lines1, &lines2, lines1.len(), lines2.len());
 }
 
 #[cfg(test)]
--- week2/rwc/src/main.rs
+++ week2/rwc/src/main.rs
@@ -1,5 +1,32 @@
 use std::env;
 use std::process;
+use std::fs::File; // For read_file_lines()
+use std::io::{self, BufRead}; // For read_file_lines()
+
+fn read_wc(filename: &String) {
+    let mut words = 0;
+    let mut characters = 0;
+    let mut lines = 0;
+    // use ? is better than unwrap()
+    let file = File::open(filename).expect("open file error.");
+    for line in io::BufReader::new(file).lines() {
+        let line_str: Vec<char> = line.expect("read error").chars().collect();
+
+        // do something with line_str
+        lines += 1;
+        for i in 0..line_str.len() {
+            if line_str[i] == ' ' {
+                words += 1;
+            } else if line_str[i].is_ascii_alphabetic() {
+                characters += 1;
+            }
+        }
+
+    }
+    // for the last word in the lines
+    words += lines; 
+    println!("Find {} words, {} lines, {} characters in file {}", words, lines, characters, filename);
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,4 +36,5 @@ fn main() {
     }
     let filename = &args[1];
     // Your code here :)
+    read_wc(filename);
 }
