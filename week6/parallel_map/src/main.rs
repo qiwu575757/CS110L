9b80136018c0cd3f1619a6b266d99c943c4a15b9
qiwu575757 <qiwu575757@gmail.com>
2022-12-12 16:01:53 +0800

    finish week6 about channel

--- week6/parallel_map/src/main.rs
+++ week6/parallel_map/src/main.rs
@@ -8,7 +8,48 @@ where
     U: Send + 'static + Default,
 {
     let mut output_vec: Vec<U> = Vec::with_capacity(input_vec.len());
+    
     // TODO: implement parallel map!
+    let (args_sender, args_receiver) = crossbeam_channel::unbounded();
+    let (result_sender, result_receiver) = crossbeam_channel::unbounded();
+
+    let mut threads = Vec::new();
+    // spawn worker threads
+    for _ in 0..num_threads {
+        let args_receiver = args_receiver.clone();
+        let result_sender = result_sender.clone();
+        threads.push(thread::spawn(move || {
+            while let Ok(next_pair) = args_receiver.recv() {
+                let (index, value) = next_pair; 
+                result_sender.send((index, f(value))).expect("Sending computed result error.");
+            }
+        }));
+    }
+
+    // send args with index
+    let input_vec_len = input_vec.len();
+    for index in 0..input_vec_len {
+        args_sender
+            .send((input_vec_len-index-1, input_vec.pop().unwrap()))
+            .expect("Tried writing to channel, but there are no args_receivers!");
+    }
+    
+    // drop the sender for receivers to know that there’s nothing more to receive
+    drop(args_sender);
+    drop(result_sender);
+
+    // init output_vec 
+    output_vec.resize_with(input_vec_len, Default::default);
+    while let Ok(result_pair) = result_receiver.recv() {
+        let (index, f_value) = result_pair;
+        output_vec[index] = f_value; 
+    }
+
+    // join all worker threads
+    for thread in threads {
+        thread.join().expect("Panic occurred in thread");
+    }
+
     output_vec
 }
 
